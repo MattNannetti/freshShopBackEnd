351a8232a73feddfabbfdfc8a8a93c794915cdbe
Matt Nannetti <mattnannetti@gmail.com>
2020-10-08 20:18:05 +0200

    proccess.env.NODE_ENV

--- server.js
+++ server.js
@@ -3,9 +3,9 @@
 // mongodb+srv://mattn:pVMrHdpJRAP9RJm0@cluster0.4opmd.mongodb.net/<dbname>?retryWrites=true&w=majority
 
 
-//if (process.env.NODE_ENV !== 'production'){
-//    require('dotenv').config();
-//}
+if (process.env.NODE_ENV !== 'production'){
+    require('dotenv').config();
+}
 
 const express = require('express');
 const app = express();
